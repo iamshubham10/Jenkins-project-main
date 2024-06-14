a2f1b7c53d58ae85713eefb4ecb8c55f04517681
Shubham Alchetti <alchettishubham@gmail.com>
2024-06-14 20:02:47 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    
+    public Student(){
+        System.out.println("Addded new Student");
+    }
+    public void display(){
+        System.out.println("Hi, I'm Student");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Captain");
+
+		System.out.println("----------------------");
+		
+		System.out.println(new Date());
+
+		Student student= new Student();
+		student.display();
+		System.out.println("----------------------");
+		
+		System.out.println("checking the code");
+	}
+
+}
